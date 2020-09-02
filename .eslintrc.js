004ebc52d56ea2c9239c1aac8bbe26c62826caf8
simonelopess <sisilopes20@gmail.com>
2020-09-02 17:12:13 -0300

    Criação de rotas

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'prettier/jsx-filename-extension': [
+      'warn',
+      {
+        extension: ['.jsx', '.js']
+      }
+    ],
+    'import/prefer-default-expoirt': 'off'
+  },
+};
