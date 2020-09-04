217199740aea724a3153f4055b6ca00c937ca425
simonelopess <sisilopes20@gmail.com>
2020-09-04 20:53:34 -0300

    Estilizando página Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import {FaGitAlt, FaPlus} from 'react-icons/fa'
 
-import { Title } from './styles';
+import { Container, Form, SubmitButton } from './styles';
 
-function Main() {
-  return <Title>
-    Hello World
-  </Title>;
+
+
+
+export default function Main() {
+  return <Container>
+    <h1>
+      <FaGitAlt />
+      Repositórios
+    </h1>
+    <Form onSubmit={() => {}}>
+      <input
+        type="text"
+        placeholder="Adicionar repositório"
+      />
+      <SubmitButton>
+        <FaPlus color="#FFF" size={14} />
+      </SubmitButton>
+    </Form>
+  </Container>;
 }
 
-export default Main;
+
