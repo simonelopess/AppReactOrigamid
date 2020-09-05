ce287c3e65c45fce26d0e9368f93983579e705e8
simonelopess <sisilopes20@gmail.com>
2020-09-04 22:07:49 -0300

    Adicionando componentes

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes, css} from 'styled-components';
 
 
 export const Container = styled.div`
@@ -36,9 +36,18 @@ export const Form = styled.form`
 
 `;
 
-export const SubmitButton = styled.button.attrs({
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+export const SubmitButton = styled.button.attrs(props => ({
   type: 'submit',
-})`
+  disable: props.loading
+}))`
   background: #7159c1;
   border: 0;
   padding: 0 15px;
@@ -48,4 +57,14 @@ export const SubmitButton = styled.button.attrs({
   display: flex;
   justify-content: center;
   align-items: center;
+
+  &[disable]{
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+
+${props =>props.loading && css `
+svg {
+    animation: ${rotate} 2s linear infinite;
+  `}
 `;
